d6113b91b3b28940d692b1d280bc0f59c24d87a9
xuechong <xuechong87@gmail.com>
2013-08-06 17:05:58 +0800

    add biz

---
+++ function-cardmanage/src/main/com/nbcedu/function/cardmanage/biz/CMCardTypeBiz.java
@@ -0,0 +1,8 @@
+package com.nbcedu.function.cardmanage.biz;
+
+import com.nbcedu.function.cardmanage.core.biz.BaseBiz;
+import com.nbcedu.function.cardmanage.model.CMCardType;
+
+public interface CMCardTypeBiz extends BaseBiz<CMCardType>{
+
+}
