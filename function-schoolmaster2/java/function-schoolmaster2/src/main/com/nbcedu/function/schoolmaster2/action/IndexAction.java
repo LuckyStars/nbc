1ae14e1f92c9c1ea275fd4a9b90b708125f59614
jonathanwl <313719427@qq.com>
2013-09-12 21:38:15 +0800

    comit action

--- function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/action/IndexAction.java
+++ function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/action/IndexAction.java
@@ -11,12 +11,22 @@ import com.nbcedu.function.schoolmaster2.utils.Utils;
  */
 @SuppressWarnings("serial")
 public class IndexAction extends BaseAction{
-	
+	private String rightURL;
 	private static final Logger logger = Logger.getLogger(IndexAction.class);
 	
 	public String index(){
 		logger.info(Utils.curUserUid());
 		return "index";
+	}
+	public String home(){
+		
+		return "home";
+	}
+	public String getRightURL() {
+		return rightURL;
+	}
+	public void setRightURL(String rightURL) {
+		this.rightURL = rightURL;
 	}
 	
 }
