b438d48f2c772e08bed0cbd215b4bb5ac7734556
xuechong <xuechong87@gmail.com>
2013-09-27 16:56:35 +0800

    add photo

--- function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/action/IndexAction.java
+++ function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/action/IndexAction.java
@@ -1,8 +1,16 @@
 package com.nbcedu.function.schoolmaster2.action;
 
+import java.sql.SQLException;
+
+import org.apache.commons.lang.xwork.StringUtils;
 import org.apache.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.springframework.orm.hibernate3.HibernateCallback;
 
 import com.nbcedu.function.schoolmaster2.core.action.BaseAction;
+import com.nbcedu.function.schoolmaster2.data.util.HibernateDao;
 
 /**
  * 
@@ -13,14 +21,18 @@ public class IndexAction extends BaseAction{
 	
 	private String rightURL;
 	private static final Logger logger = Logger.getLogger(IndexAction.class);
-	
+	private String photoPath ;
+	private HibernateDao dao;
 	
 	public String index(){
+		this.photoPath = this.getPhoto();
+		if(logger.isInfoEnabled()){
+			logger.info(photoPath);
+		}
 		return "index";
 	}
 	
 	public String home(){
-		
 		return "home";
 	}
 	
@@ -28,11 +40,36 @@ public class IndexAction extends BaseAction{
 		return "teacherInput";
 	}
 	
+	private String getPhoto(){
+		final String uid = this.getUserId();
+		String photo = (String) dao.getHibernateTemplate().execute(new HibernateCallback() {
+			@Override
+			public Object doInHibernate(Session session) throws HibernateException,
+					SQLException {
+				Query q = session.getNamedQuery("index_photo");
+				q.setString("uid", uid);
+				return q.uniqueResult();
+			}
+		});
+		return StringUtils.trimToEmpty(photo);
+	}
+	////////////////////////////////
+	/////getters&setters//////
+	/////////////////////////////
 	public String getRightURL() {
 		return rightURL;
 	}
 	public void setRightURL(String rightURL) {
 		this.rightURL = rightURL;
+	}
+	public void setPhotoPath(String photoPath) {
+		this.photoPath = photoPath;
+	}
+	public void setDao(HibernateDao dao) {
+		this.dao = dao;
+	}
+	public String getPhotoPath() {
+		return photoPath;
 	}
 	
 }
