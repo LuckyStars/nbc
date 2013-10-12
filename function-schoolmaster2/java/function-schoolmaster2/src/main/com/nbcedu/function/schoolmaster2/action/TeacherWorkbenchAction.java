f2fd1b69f00d664fceebaec5baac24fc9ea92e65
jonathanwl <313719427@qq.com>
2013-10-12 09:56:13 +0800

    add teacher

---
+++ function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/action/TeacherWorkbenchAction.java
@@ -0,0 +1,10 @@
+package com.nbcedu.function.schoolmaster2.action;
+
+import com.nbcedu.function.schoolmaster2.core.action.BaseAction;
+
+public class TeacherWorkbenchAction extends BaseAction{
+
+	private static final long serialVersionUID = 1299146396579987903L;
+	
+	Module m;
+}
--- function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/utils/UCService.java
+++ function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/utils/UCService.java
@@ -1,13 +1,27 @@
 package com.nbcedu.function.schoolmaster2.utils;
 
+import java.util.List;
+
 import org.apache.commons.lang.xwork.StringUtils;
 
 import com.nbcedu.integration.uc.client.facade.BaseClient;
+import com.nbcedu.integration.uc.client.vo.NbcUcDiction;
 
 public class UCService {
 	private static final BaseClient client = new BaseClient();
 	
 	public static String findNameByUid(String uid){
 		return StringUtils.trimToEmpty(client.queryPerson(1, uid).getName());
+	}
+	
+	public static boolean findIdentity(String uid){
+		List<NbcUcDiction> list = client.queryIdentity(uid, 1);
+		for(NbcUcDiction n : list){
+			System.out.println(n.getTypeName());
+//			if(n.g){
+//				return true;
+//			}
+		}
+		return false;
 	}
 }
