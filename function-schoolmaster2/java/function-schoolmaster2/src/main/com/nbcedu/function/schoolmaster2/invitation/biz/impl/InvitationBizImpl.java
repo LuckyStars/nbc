f1419755b39189fcebbcd5046184d734d84d0c84
xuechong <xuechong87@gmail.com>
2013-10-23 16:43:14 +0800

    remove inv

--- function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/invitation/biz/impl/InvitationBizImpl.java
+++
@@ -1,9 +0,0 @@
-package com.nbcedu.function.schoolmaster2.invitation.biz.impl;
-
-import com.nbcedu.function.schoolmaster2.core.biz.impl.BaseBizImpl;
-import com.nbcedu.function.schoolmaster2.invitation.biz.InvitationBiz;
-import com.nbcedu.function.schoolmaster2.invitation.model.SM2Invitation;
-
-public class InvitationBizImpl extends BaseBizImpl<SM2Invitation> implements InvitationBiz {
-	
-}
