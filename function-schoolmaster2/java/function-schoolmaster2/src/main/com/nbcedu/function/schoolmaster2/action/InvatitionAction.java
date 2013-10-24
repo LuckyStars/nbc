e0fb98b91aad62f5a4f519eafa0d41bfdb879156
xuechong <xuechong87@gmail.com>
2013-10-24 16:28:08 +0800

    add config

--- function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/action/InvatitionAction.java
+++ function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/action/InvatitionAction.java
@@ -5,9 +5,22 @@ import com.nbcedu.function.schoolmaster2.data.model.TSm2Subject;
 
 @SuppressWarnings("serial")
 public class InvatitionAction extends BaseAction{
+
 	private TSm2Subject subject;
-	
 
 	
-
+	public void add(){
+		
+		
+	}
+	
+	/////////////////////////
+	///getter setter//////
+	/////////////////////
+	public TSm2Subject getSubject() {
+		return subject;
+	}
+	public void setSubject(TSm2Subject subject) {
+		this.subject = subject;
+	}
 }
