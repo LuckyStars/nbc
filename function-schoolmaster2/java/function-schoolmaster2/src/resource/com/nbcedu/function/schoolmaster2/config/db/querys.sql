33771ac1f75d0ae425cfe12118050e033b4e9ca8
xuechong <xuechong87@gmail.com>
2013-11-14 17:14:08 +0800

    add find by users sql

--- function-schoolmaster2/java/function-schoolmaster2/src/resource/com/nbcedu/function/schoolmaster2/config/db/querys.sql
+++ function-schoolmaster2/java/function-schoolmaster2/src/resource/com/nbcedu/function/schoolmaster2/config/db/querys.sql
@@ -39,8 +39,85 @@ ORDER BY
 ----查询多个
 
 
+SELECT A.*
+FROM 
+	(
+		SELECT 
+			sub.id as subId,
+			sub.title as subTitle,
+			sub.createrId as createrId,
+			sub.createrName as createrName,
+			subtype.name as typeName,
+			subtype.id as typeId
+
+		FROM t_sm2_subject sub,
+			(
+				SELECT id,name
+				FROM t_sm2_type	
+			) subtype,
+			(
+				SELECT sub_id
+				FROM t_sm2_subject_master
+				WHERE user_uid = '1'
+			) submaster
+
+		WHERE
+			sub.id = submaster.sub_id
+			AND sub.status = 'new'
+			AND (DATE(sub.createTime)='2013-10-25' OR DATE(sub.lastUpdateTime)='2013-10-25')
+			AND (sub.createTime > '2012-10-10' OR sub.lastUpdateTime > '2012-10-10') 
+			AND sub.typeId = subtype.id
+			AND sub.typeId in ('1','2','3','4','5')
+			AND sub.createrId = 'J201108251334440044953'
+
+		ORDER BY 
+			sub.createTime DESC ,
+			sub.lastUpdateTime DESC
+		LIMIT 0,5
+	) A
 
 
+UNION ALL
+
+SELECT B.*
+
+FROM 
+	(
+		SELECT 
+			sub.id as subId,
+			sub.title as subTitle,
+			sub.createrId as createrId,
+			sub.createrName as createrName,
+			subtype.name as typeName,
+			subtype.id as typeId
+
+		FROM t_sm2_subject sub,
+			(
+				SELECT id,name
+				FROM t_sm2_type	
+			) subtype,
+			(
+				SELECT sub_id
+				FROM t_sm2_subject_master
+				WHERE user_uid = '1'
+			) submaster
+
+		WHERE
+			sub.id = submaster.sub_id
+			AND sub.status = 'updated'
+			AND (DATE(sub.createTime)='2013-10-25' OR DATE(sub.lastUpdateTime)='2013-10-25')
+			AND (sub.createTime > '2012-10-10' OR sub.lastUpdateTime > '2012-10-10') 
+			AND sub.typeId = subtype.id
+			AND sub.typeId in ('1','2','3','4','5')
+			AND sub.createrId = 'J201108251334440044953'
+
+		ORDER BY 
+			sub.createTime DESC ,
+			sub.lastUpdateTime DESC
+		LIMIT 0,5
+
+	) B
+;
 
 
 
