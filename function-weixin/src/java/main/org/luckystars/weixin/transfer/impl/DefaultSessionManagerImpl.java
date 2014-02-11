b9356405091f5296a5bbf7c4d884c018c604ea3e
xuechong <xuechong87@gmail.com>
2014-02-11 18:07:45 +0800

    add remove

--- function-weixin/src/java/main/org/luckystars/weixin/transfer/impl/DefaultSessionManagerImpl.java
+++ function-weixin/src/java/main/org/luckystars/weixin/transfer/impl/DefaultSessionManagerImpl.java
@@ -1,6 +1,7 @@
 package org.luckystars.weixin.transfer.impl;
 
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.luckystars.weixin.transfer.interfaces.Session;
@@ -8,13 +9,60 @@ import org.luckystars.weixin.transfer.interfaces.SessionManager;
 
 public class DefaultSessionManagerImpl implements SessionManager{
 	
+	private Thread cleanThread  = null;
+	
 	private Map<String, Session> sessions = new ConcurrentHashMap<String, Session>();
 
-	@Override
-	public Session getSession(String id) {
-		return sessions.get(id);
+	public DefaultSessionManagerImpl (){
+		initClean();
 	}
 	
 	
+	@Override
+	public Session getSession(String id) {
+		Session result = sessions.get(id);
+		if(result==null){
+			result = createSession(id);
+		}
+		return sessions.get(id);
+	}
+
+	@Override
+	public Session createSession(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
+	@Override
+	public void removeSession(String id){
+		this.sessions.remove(id);//因为使用了ConcurrentHashMap不用同步锁
+	}
+	
+	private final void initClean(){
+		this.cleanThread = new Thread(new SessionKiller(this));
+		this.cleanThread.start();
+	}
+	
+	/**
+	 * 用于清除过期session
+	 * @author xuechong
+	 */
+	private static class SessionKiller implements Runnable{
+
+		private DefaultSessionManagerImpl sessionManager ;
+		
+		public SessionKiller(DefaultSessionManagerImpl sm){
+			this.sessionManager = sm;
+		}
+		
+		@Override
+		public void run() {
+			synchronized (this.sessionManager.sessions) {
+				for (Entry<String, Session> entry:this.sessionManager.sessions.entrySet()) {
+					
+				}
+			}
+		}
+		
+	}
 }
--- function-weixin/src/java/main/org/luckystars/weixin/transfer/interfaces/SessionManager.java
+++ function-weixin/src/java/main/org/luckystars/weixin/transfer/interfaces/SessionManager.java
@@ -4,5 +4,9 @@ public interface SessionManager {
 
 	public Session getSession(String id);
 
+	Session createSession(String id);
+
+	void removeSession(String id);
+
 
 }
