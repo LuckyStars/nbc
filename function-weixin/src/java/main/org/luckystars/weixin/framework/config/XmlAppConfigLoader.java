add263179eeeaf7017bcf1175061db470c043ce8
xuechong <xuechong87@gmail.com>
2014-02-13 18:22:16 +0800

    add dom read

--- function-weixin/src/java/main/org/luckystars/weixin/framework/config/XmlAppConfigLoader.java
+++ function-weixin/src/java/main/org/luckystars/weixin/framework/config/XmlAppConfigLoader.java
@@ -1,15 +1,46 @@
 package org.luckystars.weixin.framework.config;
 
+
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.log4j.Logger;
 import org.luckystars.weixin.framework.AppContext;
 import org.luckystars.weixin.framework.api.AppContextLoader;
+import org.w3c.dom.Document;
 
 public class XmlAppConfigLoader implements AppContextLoader{
 
+	private static final Logger logger = Logger.getLogger(XmlAppConfigLoader.class);
+	
 	@Override
 	public void loadIntoContext(AppContext ctx) {
 		String xmlPath = ctx.getConfigLocation();
-
+		Document document = buildDoument(xmlPath);
 		
+	}
+
+	private Document buildDoument(String xmlPath) {
+		Document result = null;
+		
+		try {
+			InputStream in = Thread.currentThread().
+				getContextClassLoader().getResourceAsStream(xmlPath);
+			if(in==null){
+				throw new NullPointerException("配置文件:" + xmlPath + "不存在");
+			}
+			DocumentBuilderFactory fac = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = fac.newDocumentBuilder();
+			result = builder.parse(in);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error(e);
+		}
+		
+		return result;
 	}
 	
 	
