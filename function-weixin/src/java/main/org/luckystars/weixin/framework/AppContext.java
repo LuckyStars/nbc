0b90dd475e22041c98ac5bf8266e52d5a30c2fc1
xuechong <xuechong87@gmail.com>
2014-02-13 09:39:15 +0800

    add app contenxt

--- function-weixin/src/java/main/org/luckystars/weixin/framework/AppContext.java
+++ function-weixin/src/java/main/org/luckystars/weixin/framework/AppContext.java
@@ -1,8 +1,36 @@
 package org.luckystars.weixin.framework;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 应用全局上下文
  * @author xuechong
  */
-public class AppContext {
+public class AppContext implements Serializable{
 
+	private static final long serialVersionUID = -5759981728727750618L;
+	
+	private static AppContext context ;
+	
+	private Map<String, Object> ctx = new HashMap<String, Object>();
+	
+	private AppContext(){
+		
+	}
+	
+	public AppContext getContext(){
+		return context;
+	}
+	
+	public Object get(String key){
+		return ctx.get(key);
+	}
+	
+	
+	
+	
+	
+	
 }
