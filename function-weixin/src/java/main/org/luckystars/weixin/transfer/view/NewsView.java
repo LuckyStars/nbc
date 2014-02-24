55a9a3dbbbc3139ed71f25ca65f9624ae71b1ca4
xuechong <xuechong87@gmail.com>
2014-02-24 17:47:45 +0800

    add items

--- function-weixin/src/java/main/org/luckystars/weixin/transfer/view/NewsView.java
+++ function-weixin/src/java/main/org/luckystars/weixin/transfer/view/NewsView.java
@@ -1,11 +1,22 @@
 package org.luckystars.weixin.transfer.view;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.luckystars.weixin.transfer.interfaces.XmlReply;
 
+@SuppressWarnings("serial")
 public class NewsView implements XmlReply{
 
 	private String fromUserName;
 	private String toUserName;
+	private String createTime;
+	private String msgType = "news";
+	/**图文消息个数，限制为10条以内***/
+	private String articleCount;
+	
+	private List<NewsView.Item> items = new ArrayList<NewsView.Item>(10);
 	
 	@Override
 	public String toWeixinStr() {
@@ -18,5 +29,13 @@ public class NewsView implements XmlReply{
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	
+	public static class Item implements Serializable{
+		private String title;
+		private String description;
+		private String picUrl;
+		private String url;
+	}
 }
+
+
