3c9fea0d96f73b7cef92382deebacead29f6d953
xuechong <xuechong87@gmail.com>
2014-03-07 17:55:35 +0800

    add list test

--- function-weixin/src/java/main/org/luckystars/weixin/framework/handlers/TestHandler.java
+++ function-weixin/src/java/main/org/luckystars/weixin/framework/handlers/TestHandler.java
@@ -1,6 +1,9 @@
 package org.luckystars.weixin.framework.handlers;
 
 
+import java.io.Serializable;
+import java.util.ArrayList;
+
 import org.apache.log4j.Logger;
 import org.luckystars.weixin.framework.HandlerInvocation;
 import org.luckystars.weixin.framework.api.HandleResult;
@@ -9,6 +12,12 @@ import org.luckystars.weixin.framework.api.IncomeMessage;
 import org.luckystars.weixin.framework.api.WeixinView;
 import org.luckystars.weixin.transfer.incomemsg.WeixinMsg;
 import org.luckystars.weixin.transfer.view.TextView;
+import org.luckystars.weixin.transfer.view.annotations.AnnotationNewsViewBuilder;
+import org.luckystars.weixin.transfer.view.annotations.Description;
+import org.luckystars.weixin.transfer.view.annotations.Item;
+import org.luckystars.weixin.transfer.view.annotations.PicUrl;
+import org.luckystars.weixin.transfer.view.annotations.Title;
+import org.luckystars.weixin.transfer.view.annotations.Url;
 
 public class TestHandler implements Handler{
 
@@ -20,7 +29,7 @@ public class TestHandler implements Handler{
 		IncomeMessage msg = invocation.getInvocationContext().getMsg();
 		
 		if(msg instanceof WeixinMsg){
-			WeixinMsg m = (WeixinMsg)msg;
+			final WeixinMsg m = (WeixinMsg)msg;
 			logger.info("\n\n" + m.getRawXml() + "\n\n");
 			String toUsr = m.getFromUserName();
 			String serverName = m.getToUserName();
@@ -31,11 +40,31 @@ public class TestHandler implements Handler{
 				public void setView(WeixinView view) {
 				}
 				public WeixinView getView() {
-					return view;
+					return new AnnotationNewsViewBuilder().build(new ArrayList<TestList>(){{
+						add(new TestList());
+						add(new TestList());
+						add(new TestList());
+					}}, m);
 				}
 			};
+			
 		}
 		return invocation.invokeNext();
 	}
 	
+	@Item
+	private static class TestList implements Serializable {
+		@PicUrl
+		private String picurl="bbbb.bbbbb";
+		@Url
+		private String url="aaaa.aaaaa";
+		@Title
+		private String title = "title";
+		@Description
+		private String desc = "desc";
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
