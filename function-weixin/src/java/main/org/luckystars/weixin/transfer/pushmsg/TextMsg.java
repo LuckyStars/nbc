0d27f55401fc23470ffdbcda0bd192477a19f9fb
xuechong <xuechong87@gmail.com>
2014-03-07 09:50:41 +0800

    add textmsg to json

--- function-weixin/src/java/main/org/luckystars/weixin/transfer/pushmsg/TextMsg.java
+++ function-weixin/src/java/main/org/luckystars/weixin/transfer/pushmsg/TextMsg.java
@@ -1,16 +1,58 @@
 package org.luckystars.weixin.transfer.pushmsg;
 
+import org.luckystars.weixin.utils.json.GsonUtil;
+
+@SuppressWarnings("serial")
 public class TextMsg extends JsonOutputMsg{
 
+	private String touser;
+	private String msgtype = MSG_TYPE_TEXT;
+	private Text text;
+	
 	@Override
 	public String getMsgType() {
-		return null;
+		return msgtype;
 	}
-
 	@Override
 	String toJsonString() {
-		// TODO Auto-generated method stub
-		return null;
+		return GsonUtil.gson.toJson(this);
+	}
+	public String getTouser() {
+		return touser;
+	}
+	public void setTouser(String touser) {
+		this.touser = touser;
+	}
+	public Text getText() {
+		return text;
+	}
+	public void setText(Text text) {
+		this.text = text;
+	}
+	public void putText(String text){
+		this.text = new Text(text);
 	}
 
+	public static class Text{
+		public Text(){super();};
+		public Text(String content) {
+			super();
+			this.content = content;
+		}
+		private String content;
+		public String getContent() {
+			return content;
+		}
+		public void setContent(String content) {
+			this.content = content;
+		}
+	}
+	
+	public static void main(String[] args) {
+		TextMsg t = new TextMsg();
+		t.setText(new Text("aasdsadasd"));
+		t.setTouser("touserId11234");
+		System.out.println(t.toJsonString());
+	}
 }
+
