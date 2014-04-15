eaea3f6950feeacdf067994a9ea1d98d2bf5b59a
xuechong <xuechong87@gmail.com>
2014-04-15 09:28:01 +0800

    add jar pom

--- function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/tests/AntlrTests.java
+++
@@ -1,5 +0,0 @@
-package com.nbcedu.function.schoolmaster2.tests;
-
-
-import com.nbcedu.function.schoolmaster2.tags.display.AbstractDisplayTag;
-
---
+++ function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/tests/Weixin.java
@@ -0,0 +1,7 @@
+package com.nbcedu.function.schoolmaster2.tests;
+
+public class Weixin {
+	public static void main(String[] args) {
+		
+	}
+}
