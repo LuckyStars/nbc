8c8cd6dc6c389003ff5f4cba29abe3e84bf885fe
xuechong <xuechong87@gmail.com>
2014-04-16 15:26:19 +0800

    add biz dao

---
+++ function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/weixin/biz/Sm2WeixinUserBiz.java
@@ -0,0 +1,8 @@
+package com.nbcedu.function.schoolmaster2.weixin.biz;
+
+import com.nbcedu.function.schoolmaster2.core.biz.BaseBiz;
+import com.nbcedu.function.schoolmaster2.weixin.model.Sm2WeixinUser;
+
+public interface Sm2WeixinUserBiz extends BaseBiz<Sm2WeixinUser> {
+
+}
---
+++ function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/weixin/dao/impl/Sm2WeixinUserDaoImpl.java
@@ -0,0 +1,9 @@
+package com.nbcedu.function.schoolmaster2.weixin.dao.impl;
+
+import com.nbcedu.function.schoolmaster2.core.dao.impl.SimpleBaseDAOImpl;
+import com.nbcedu.function.schoolmaster2.weixin.dao.Sm2WeixinUserDao;
+import com.nbcedu.function.schoolmaster2.weixin.model.Sm2WeixinUser;
+
+public class Sm2WeixinUserDaoImpl extends SimpleBaseDAOImpl<Sm2WeixinUser> implements Sm2WeixinUserDao {
+
+}
