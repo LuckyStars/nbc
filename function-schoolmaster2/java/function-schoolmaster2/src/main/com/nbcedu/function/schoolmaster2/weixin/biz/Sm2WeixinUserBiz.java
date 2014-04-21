8f4ec2a8ddcde4617932d9ec5c4e03cc96e4154d
wl <jonathan_wl>
2014-04-21 14:16:06 +0800

    user bind

--- function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/weixin/biz/Sm2WeixinUserBiz.java
+++ function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/weixin/biz/Sm2WeixinUserBiz.java
@@ -4,5 +4,21 @@ import com.nbcedu.function.schoolmaster2.core.biz.BaseBiz;
 import com.nbcedu.function.schoolmaster2.weixin.model.Sm2WeixinUser;
 
 public interface Sm2WeixinUserBiz extends BaseBiz<Sm2WeixinUser> {
+	/**
+	 * 增加、重新绑定用户
+	 * @param user
+	 */
+	public void addUpdateWeixinUser(Sm2WeixinUser user);
+	/**
+	 * 根据id逻辑删除用户
+	 * @param id
+	 */
+	public void modifyWeixinUserById(String id);
+	/**
+	 * 根据openId逻辑删除用户
+	 * @param openId
+	 */
+	public void modifyWeixinUserByOpenId(String openId);
 
+	
 }
--- function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/weixin/biz/impl/Sm2WeixinUserBizImpl.java
+++ function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/weixin/biz/impl/Sm2WeixinUserBizImpl.java
@@ -1,6 +1,7 @@
 package com.nbcedu.function.schoolmaster2.weixin.biz.impl;
 
 import com.nbcedu.function.schoolmaster2.core.biz.impl.BaseBizImpl;
+import com.nbcedu.function.schoolmaster2.core.util.StringUtil;
 import com.nbcedu.function.schoolmaster2.weixin.biz.Sm2WeixinUserBiz;
 import com.nbcedu.function.schoolmaster2.weixin.dao.Sm2WeixinUserDao;
 import com.nbcedu.function.schoolmaster2.weixin.model.Sm2WeixinUser;
@@ -13,6 +14,52 @@ public class Sm2WeixinUserBizImpl extends BaseBizImpl<Sm2WeixinUser> implements 
 		this.weixinUserDao = weixinUserDao;
 		
 	}
+
+	@Override
+	public void addUpdateWeixinUser(Sm2WeixinUser user) {
+		if(!StringUtil.isEmpty(user.getWeixinId())){
+			Sm2WeixinUser u = this.findWeixinUser(user.getWeixinId());
+			if(u!=null){
+				updateWeixinUser(0,user.getWeixinId());
+			}else{
+				
+			}
+		}
+		
+	}
+	private int updateWeixinUser(int status,String weixinId){
+		String hql  = "update Sm2WeixinUser set status = ? where weixinId = ?";
+		return this.weixinUserDao.createQuery(hql,status, weixinId).executeUpdate();
+	}
+	/**
+	 * 增加新用户
+	 * @param user
+	 */
+	public void addWeixinUser(Sm2WeixinUser user) {
+		this.weixinUserDao.save(user);
+	}
+	@Override
+	public void modifyWeixinUserById(String id) {
+		String hql  = "update Sm2WeixinUser set status = 0 where id = ?";
+		this.weixinUserDao.createQuery(hql, id).executeUpdate();
+		
+	}
+
+	@Override
+	public void modifyWeixinUserByOpenId(String weixinId) {
+		String hql  = "update Sm2WeixinUser set status = 0 where weixinId = ?";
+		this.weixinUserDao.createQuery(hql, weixinId).executeUpdate();
+		
+	}
 	
+
+	/**
+	 * 查询用户根据weixinId
+	 * @param weixinId
+	 * @return
+	 */
+	public Sm2WeixinUser findWeixinUser(String weixinId){
+		return this.weixinUserDao.findUniqueBy("weixinId", weixinId);
+	}
 	
 }
