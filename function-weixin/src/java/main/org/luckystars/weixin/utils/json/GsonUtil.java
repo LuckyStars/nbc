591f6637f99c4ab1dd1cc2c2ead15fac765d1e9e
xuechong <xuechong87@gmail.com>
2014-04-29 16:16:24 +0800

    add access token fetch

--- function-weixin/src/java/main/org/luckystars/weixin/utils/json/GsonUtil.java
+++ function-weixin/src/java/main/org/luckystars/weixin/utils/json/GsonUtil.java
@@ -1,7 +1,16 @@
 package org.luckystars.weixin.utils.json;
 
+import java.util.Map;
+
+import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 
 public class GsonUtil {
 	public static final Gson gson = new Gson();
+	
+	@SuppressWarnings("serial")
+	public static Map<String, String> fromJson(String json){
+		return gson.fromJson(json, 
+				new TypeToken<Map<String, String>>() {}.getType());
+	}
 }
