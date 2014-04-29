755fd171f5893e09b69d83f948181ade43f77c8d
xuechong <xuechong87@gmail.com>
2014-04-29 14:28:48 +0800

    add logout handler

--- function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/weixin/handler/LogoutHandler.java
+++ function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/weixin/handler/LogoutHandler.java
@@ -3,12 +3,13 @@ package com.nbcedu.function.schoolmaster2.weixin.handler;
 import org.luckystars.weixin.framework.HandlerInvocation;
 import org.luckystars.weixin.framework.api.HandleResult;
 import org.luckystars.weixin.transfer.handler.AbstractEventHandler;
+import org.luckystars.weixin.transfer.incomemsg.WeixinMsg;
 import org.luckystars.weixin.transfer.view.TextView;
 
 import com.nbcedu.function.schoolmaster2.weixin.biz.Sm2WeixinUserBiz;
 
 /**
- * 注销登陆
+ * 注销登陆 
  * @author xuechong
  */
 public class LogoutHandler extends AbstractEventHandler{
@@ -21,7 +22,8 @@ public class LogoutHandler extends AbstractEventHandler{
 	protected HandleResult doHandle(HandlerInvocation invocation) {
 		String openId = invocation.getInvocationContext().getSession().getSessionId();
 		this.wxUserBiz.logOut(openId);
-		return invocation.createResult(new TextView("注销成功", toUserName, serverName));
+		WeixinMsg msg = (WeixinMsg) invocation.getIncomeMsg();
+		return invocation.createResult(new TextView("注销成功", msg));
 	}
 
 	@Override
