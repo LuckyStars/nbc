06170be20255c03b6a699181b389eef7db5b9cd9
xuechong <xuechong87@gmail.com>
2014-05-05 11:16:34 +0800

    modify msgtester

--- msgtester/helloworld.py
+++ msgtester/helloworld.py
@@ -5,14 +5,20 @@ import logging
 
 class Main(webapp2.RequestHandler):
     
+    @staticmethod
+    def writeParams(req):
+        for arg in req.arguments():
+            logging.info("param :==> " + str(arg) + ":" + str(req.get(arg)))
+        pass
+    
     def get(self):
         self.post()
         
     def post(self):
-        param = self.request.get
+        #param = self.request.get
         write = self.response.out.write
-        logging.info(param)
-        logging.info(self.request._body__get())
+        Main.writeParams(self.request)
+        logging.info("request body :==> " + self.request._body__get())
         self.response.headers['Content-Type'] = 'text/plain'
         write("200 OK")
 
