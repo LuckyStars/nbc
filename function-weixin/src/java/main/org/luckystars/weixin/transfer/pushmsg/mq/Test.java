4adbe1c9c69041e570b5d5585881a0b29545694d
xuechong <xuechong87@gmail.com>
2014-05-07 16:55:27 +0800

    add mqtests

---
+++ function-weixin/src/java/main/org/luckystars/weixin/transfer/pushmsg/mq/Test.java
@@ -0,0 +1,82 @@
+package org.luckystars.weixin.transfer.pushmsg.mq;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.jms.Connection;
+import javax.jms.Destination;
+
+import org.apache.activemq.broker.BrokerPlugin;
+import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.network.jms.JmsConnector;
+import org.apache.activemq.security.AuthenticationUser;
+import org.apache.activemq.security.SimpleAuthenticationPlugin;
+
+public class Test {
+	
+	static final String brokerUrl = "vm://localhost";
+	
+	@SuppressWarnings("serial")
+	public static void main(String[] args) {
+		Function startCallBack1 = new Function() {
+			@Override
+			void execute() {
+				System.out.println("server started");
+			}
+		};
+		
+		
+		Thread serverThread = new Thread(new StartServer(startCallBack1));
+		serverThread.start();
+	}
+	
+	private static void setUpConsumer(){
+		
+	}
+	
+	
+	
+	
+	public static abstract class Function{
+		abstract void execute();
+	}
+	
+	
+	public static class StartServer implements Runnable{
+		private Function callBack;
+		StartServer(Function startCallBack){
+			this.callBack = startCallBack;
+		}
+		@Override
+		public void run() {
+			BrokerService broker = new BrokerService();
+			broker.setBrokerName("testBroker");
+			broker.setDataDirectory("mqdata/");
+		
+			List<AuthenticationUser> users = new ArrayList<AuthenticationUser>(){{
+				add(new AuthenticationUser("user1", "pwd1", "consumers"));
+				add(new AuthenticationUser("user2", "pwd2", "consumers"));
+				add(new AuthenticationUser("user3", "pwd3", "consumers"));
+			}};
+
+			SimpleAuthenticationPlugin auth = new SimpleAuthenticationPlugin(users);
+			
+			broker.setPlugins(new BrokerPlugin[]{auth});
+			
+			
+			try {
+				broker.addConnector(brokerUrl);
+				broker.addConnector("stomp://localhost:61616");
+				broker.start();
+
+				this.callBack.execute();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+	}
+	
+	
+	
+}
