0db034bf661db432334d38424e759c89e2594e93
xuechong <xuechong87@gmail.com>
2014-05-15 09:41:44 +0800

    add news view constractor

--- function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/weixin/handler/LoginHandler.java
+++ function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/weixin/handler/LoginHandler.java
@@ -13,6 +13,7 @@ import org.luckystars.weixin.framework.api.View;
 import org.luckystars.weixin.framework.config.xml.AppUrlLoader;
 import org.luckystars.weixin.transfer.incomemsg.EventMsg;
 import org.luckystars.weixin.transfer.incomemsg.WeixinMsg;
+import org.luckystars.weixin.transfer.view.NewsView;
 import org.luckystars.weixin.transfer.view.TextView;
 
 import com.nbcedu.function.schoolmaster2.weixin.biz.Sm2WeixinUserBiz;
@@ -29,6 +30,7 @@ public class LoginHandler implements Handler{
 	private Sm2WeixinUserBiz wxUserBiz ;
 	
 	private final String loginUrl = "/function/function-weixin/index.jsp";
+	private final String loginPicUrl = "/function/function-weixin/images/login.jpg";
 	
 	@Override
 	public HandleResult handle(HandlerInvocation invocation) throws Exception {
@@ -56,18 +58,28 @@ public class LoginHandler implements Handler{
 		return invocation.invokeNext();
 	}
 	
+	private String getCtxPath(){
+		return AppContext.getContext().get(AppUrlLoader.APP_URL_KEY).toString();
+	}
+	
+	private String getLoginPidUrl(){
+		return getCtxPath() + loginPicUrl;
+	}
+	
 	private String getUserOpenId(){
 		return HandlerContext.getContext().getSession().getSessionId();
 	}
 	
 	private String getUserLoginUrl(){
-		String ctxPath = AppContext.getContext().get(AppUrlLoader.APP_URL_KEY).toString();
-		return ctxPath + loginUrl + "?openId="+getUserOpenId() + "&timestamp=" + System.currentTimeMillis();
+		return getCtxPath() + loginUrl + "?openId="+getUserOpenId() + "&timestamp=" + System.currentTimeMillis();
 	}
 	
 	private View createLoginView(WeixinMsg msg) {
 		View view = new TextView("您尚未登录系统\n<a href=\"" 
 				+ getUserLoginUrl() + "\">点此登录</a>",msg);
+		
+		NewsView nview = new NewsView();
+		
 		return view;
 	}
 
