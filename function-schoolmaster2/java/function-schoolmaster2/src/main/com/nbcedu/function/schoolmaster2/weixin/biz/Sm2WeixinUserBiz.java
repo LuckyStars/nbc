66f2e847e324a23aa0f281b07d53dde0c95e04d3
yuanwang <yuanwang0411@126.com>
2014-05-22 10:01:21 +0800

    'ok'

--- function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/weixin/biz/Sm2WeixinUserBiz.java
+++ function-schoolmaster2/java/function-schoolmaster2/src/main/com/nbcedu/function/schoolmaster2/weixin/biz/Sm2WeixinUserBiz.java
@@ -1,7 +1,6 @@
 package com.nbcedu.function.schoolmaster2.weixin.biz;
-
-import com.nbcedu.function.schoolmaster2.core.biz.BaseBiz;
 import com.nbcedu.function.schoolmaster2.weixin.model.Sm2WeixinUser;
+import com.nbcedu.function.schoolmaster2.core.biz.BaseBiz;
 
 public interface Sm2WeixinUserBiz extends BaseBiz<Sm2WeixinUser> {
 	/**
